ce22595154094bb2cc05b1eb3bb55b699463d56f
Abdullah Aldawsari <aldawsari@Abdullahs-MacBook-Air.local>
2026-04-11 09:29:09 +0300

    feat: add menu bar status item with note.text icon

--- Sources/Notaty/AppDelegate.swift
+++ Sources/Notaty/AppDelegate.swift
@@ -1,7 +1,21 @@
 import AppKit
 
 final class AppDelegate: NSObject, NSApplicationDelegate {
+    private var statusItem: NSStatusItem!
+
     func applicationDidFinishLaunching(_ notification: Notification) {
-        NSLog("Notaty launched")
+        statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength)
+        if let button = statusItem.button {
+            button.image = NSImage(
+                systemSymbolName: "note.text",
+                accessibilityDescription: "Notaty"
+            )
+            button.target = self
+            button.action = #selector(toggleWindow)
+        }
+    }
+
+    @objc private func toggleWindow() {
+        NSLog("Notaty icon clicked")
     }
 }
